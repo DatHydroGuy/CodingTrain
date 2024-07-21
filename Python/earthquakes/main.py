8a6c186a62638fec15da4b06acfc3f48f7cc7353
DatHydroGuy <28946117+DatHydroGuy@users.noreply.github.com>
2024-07-21 21:54:42 +0100

    First attempt at rendering earthquake data in 3D using OpenGL

--- Python/earthquakes/main.py
+++ Python/earthquakes/main.py
@@ -1,4 +1,7 @@
-from earthquakes_2d import Earthquakes2D
+# from earthquakes_2d import Earthquakes2D
+import earthquakes_GL
+
 
 if __name__ == '__main__':
-    Earthquakes2D().start()
+    # Earthquakes2D().start()
+    earthquakes_GL.main()
